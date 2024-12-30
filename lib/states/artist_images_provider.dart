990b192609f189847d0eb430b8f832aeee69eb03
FalconTube <yannic.falke@gmail.com>
2024-12-30 20:25:42 +0100

    Get rid of old notifier and remove duplicates in list

--- lib/states/artist_images_provider.dart
+++ lib/states/artist_images_provider.dart
@@ -1,14 +1,11 @@
+import 'dart:collection';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jukebox_spotify_flutter/api/spotify_api.dart';
 import 'package:jukebox_spotify_flutter/classes/artist.dart';
 import 'package:jukebox_spotify_flutter/logging/pretty_logger.dart';
 
 import 'package:jukebox_spotify_flutter/states/chosen_artist_filter.dart';
-
-// 2. Create an AsyncNotifierProvider to manage the image URLs
-final imageListProvider =
-    AsyncNotifierProvider<ImageListNotifier, List<ArtistCard>>(
-        ImageListNotifier.new);
 
 // Provider for managing the data and API calls
 final dataProvider = StateNotifierProvider<DataNotifier, DataState>((ref) {
@@ -57,15 +54,26 @@ class DataNotifier extends StateNotifier<DataState> {
     if (state.isLoading) return; // Prevent concurrent requests
 
     state = state.copyWith(isLoading: true, error: null);
-    // final artistLetter = ref.watch(chosenArtistFilterProvider);
-    // final genre = ref.watch(chosenGenreFilterProvider);
-    final artists = await _getMostRelevantArtists(artistLetter, 3, genre);
+    int currentAmountItems = state.data.length;
+    final artists = await _getMostRelevantArtists(
+        artistLetter, 20, genre, currentAmountItems);
     sortByFollowersDescending(artists);
-    state = state.copyWith(
-      data: [...state.data, ...artists], // Append new data
-      isLoading: false,
-      page: state.page + 1, // Increment page number
-    );
+    removeNotStartingWithLetter(artists, artistLetter);
+    // Remove possible duplicates
+    if (currentAmountItems > 1) {
+      final distinctArtists = removeAlreadyExisting(state.data, artists);
+      state = state.copyWith(
+        data: [...state.data, ...distinctArtists], // Append new data
+        isLoading: false,
+        page: state.page + 1, // Increment page number
+      );
+    } else {
+      state = state.copyWith(
+        data: [...state.data, ...artists], // Append new data
+        isLoading: false,
+        page: state.page + 1, // Increment page number
+      );
+    }
   }
 
   //
@@ -80,16 +88,41 @@ class DataNotifier extends StateNotifier<DataState> {
     // artists.sort((a, b) => b.followers.compareTo(a.followers));
   }
 
+  List<ArtistCard> removeAlreadyExisting(
+      List<ArtistCard> curArt, List<ArtistCard> newArt) {
+    // Create list of names of artists
+    List<String> curArtNames = [];
+    for (final artist in curArt) {
+      // Log.log(artist.toString());
+      curArtNames.add(artist.name);
+    }
+    // Now check if they are already present in existing
+    List<ArtistCard> distinctArtists = [];
+    for (final artist in newArt) {
+      if (curArtNames.contains(artist.name)) {
+        continue;
+      }
+      distinctArtists.add(artist);
+    }
+
+    return distinctArtists;
+  }
+
+  List<ArtistCard> removeDuplicates(List<ArtistCard> artists) {
+    final distinctArtists = LinkedHashSet<ArtistCard>.from(artists).toList();
+    return distinctArtists;
+  }
+
   Future<List<ArtistCard>> _getMostRelevantArtists(
-      String letter, int limit, String genre) async {
+      String letter, int limit, String genre, int offset) async {
     String uri;
 
     if (genre == "") {
       uri =
-          "https://api.spotify.com/v1/search?q=$letter&type=artist&limit=$limit";
+          "https://api.spotify.com/v1/search?q=$letter&type=artist&limit=$limit&offset=$offset";
     } else {
       uri =
-          'https://api.spotify.com/v1/search?q=$letter genre:"$genre"&type=artist&limit=$limit';
+          'https://api.spotify.com/v1/search?q=$letter genre:"$genre"&type=artist&limit=$limit&offset=$offset';
     }
 
     final api = await SpotifyApiService.api;
@@ -108,74 +141,5 @@ class DataNotifier extends StateNotifier<DataState> {
           imageUrl: img, name: name, popularity: pop, followers: follows));
     }
     return foundArtists;
-  }
-}
-
-class ImageListNotifier extends AsyncNotifier<List<ArtistCard>> {
-  @override
-  Future<List<ArtistCard>> build() async {
-    // If chosenArtist changes, we must update this list
-    final artistLetter = ref.watch(chosenArtistFilterProvider);
-    final genre = ref.watch(chosenGenreFilterProvider);
-    final artists = await _getMostRelevantArtists(artistLetter, 9, genre);
-    sortByFollowersDescending(artists);
-    // removeNotStartingWithLetter(artists, artistLetter);
-    return artists;
-    // return _fetchImages(artists);
-  }
-
-  Future<void> doAll() async {
-    final artists = await _getMostRelevantArtists("A", 9, "metal");
-    sortByFollowersDescending(artists);
-  }
-
-  // Sorts all artists by their amount of followers in descending order
-  void sortByFollowersDescending(List<ArtistCard> artists) {
-    artists.sort((a, b) => b.followers.compareTo(a.followers));
-  }
-
-  // Sorts all artists by their amount of followers in descending order
-  void removeNotStartingWithLetter(List<ArtistCard> artists, String letter) {
-    artists.removeWhere((artist) => artist.name.startsWith(letter) == false);
-    // artists.sort((a, b) => b.followers.compareTo(a.followers));
-  }
-
-  Future<List<ArtistCard>> _getMostRelevantArtists(
-      String letter, int limit, String genre) async {
-    String uri;
-
-    if (genre == "") {
-      uri =
-          "https://api.spotify.com/v1/search?q=$letter&type=artist&limit=$limit";
-    } else {
-      uri =
-          'https://api.spotify.com/v1/search?q=$letter genre:"$genre"&type=artist&limit=$limit';
-    }
-
-    final api = await SpotifyApiService.api;
-    final out = await api.get(uri);
-    // 'https://api.spotify.com/v1/search?q=$letter&type=artist&limit=$limit');
-    final items = out.data["artists"]["items"];
-
-    List<ArtistCard> foundArtists = [];
-    for (final item in items) {
-      final name = item["name"].toString();
-      final pop = item["popularity"];
-      final follows = item["followers"]["total"];
-      final img = item["images"][0]["url"].toString();
-      // Log.log("Name: $name, Pop: $pop, Fol: $follows Img: $img");
-      foundArtists.add(ArtistCard(
-          imageUrl: img, name: name, popularity: pop, followers: follows));
-    }
-    return foundArtists;
-  }
-
-  Future<void> refresh() async {
-    state =
-        const AsyncLoading(); // Important for showing loading state on refresh
-    state = await AsyncValue.guard(() => _getMostRelevantArtists(
-        ref.read(chosenArtistFilterProvider),
-        4,
-        ref.read(chosenGenreFilterProvider)));
   }
 }
--- lib/widgets/artist_grid.dart
+++ lib/widgets/artist_grid.dart
@@ -44,7 +44,6 @@ class _ArtistGridState extends ConsumerState<ArtistGrid> {
   @override
   Widget build(BuildContext context) {
     final dataState = ref.watch(dataProvider);
-    final imageListAsyncValue = ref.watch(imageListProvider);
 
     if (dataState.isLoading && dataState.data.isEmpty) {
       return const Center(child: CircularProgressIndicator());
@@ -54,59 +53,54 @@ class _ArtistGridState extends ConsumerState<ArtistGrid> {
       return Center(child: Text(dataState.error!));
     }
 
-    return imageListAsyncValue.when(
-      data: (imageList) => GridView.builder(
-        controller: _scrollController,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 1,
-          crossAxisSpacing: 8,
-          mainAxisSpacing: 8,
-        ),
-        itemCount: dataState.data.length + (dataState.isLoading ? 1 : 0),
-        // itemCount: imageList.length,
-        itemBuilder: (context, index) {
-          if (index < dataState.data.length) {
-            final imageData = dataState.data[index];
-            return Card(
-              elevation: 5,
-              clipBehavior: Clip.antiAlias,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              child: InkWell(
-                onTap: () {},
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: FadeInImage.memoryNetwork(
-                        image: imageData.imageUrl,
-                        fit: BoxFit.cover,
-                        placeholder: widget.placeholder,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        imageData.name,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                        overflow: TextOverflow.ellipsis, // Handle long text
-                        maxLines: 1,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        },
+    return GridView.builder(
+      controller: _scrollController,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        crossAxisSpacing: 8,
+        mainAxisSpacing: 8,
       ),
-      // loading: () => const Center(child: CircularProgressIndicator()),
-      loading: () => const Center(),
-      error: (error, stackTrace) => Center(child: Text('Error: $error')),
+      itemCount: dataState.data.length + (dataState.isLoading ? 1 : 0),
+      // itemCount: imageList.length,
+      itemBuilder: (context, index) {
+        if (index < dataState.data.length) {
+          final imageData = dataState.data[index];
+          return Card(
+            elevation: 5,
+            clipBehavior: Clip.antiAlias,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: InkWell(
+              onTap: () {},
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: FadeInImage.memoryNetwork(
+                      image: imageData.imageUrl,
+                      fit: BoxFit.cover,
+                      placeholder: widget.placeholder,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      imageData.name,
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                      overflow: TextOverflow.ellipsis, // Handle long text
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
