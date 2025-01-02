19fed7e254f299d983e8825fad22b33480bfd451
FalconTube <yannic.falke@gmail.com>
2025-01-03 00:38:19 +0100

    Slower transitions

---
+++ lib/widgets/slow_material_route.dart
@@ -0,0 +1,8 @@
+// import 'package:flutter/material.dart';
+//
+// class SlowMaterialPageRoute extends MaterialPageRoute {
+//   SlowMaterialPageRoute({WidgetBuilder builder}) : super(builder: builder);
+//
+//   @override
+//   Duration get transitionDuration => Duration(seconds: 3);
+// }
